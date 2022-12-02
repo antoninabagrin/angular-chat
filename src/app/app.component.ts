138d94fe0544e9bb8f89df373b060adbaa30a965
antoninabagrin <antonia_bagrin@yahoo.com>
2022-12-02 19:34:48 +0200

    chat FE

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import Pusher from 'pusher-js';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  username = 'username';
+  message = '';
+  messages = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    Pusher.logToConsole = true;
+
+    const pusher = new Pusher('', {
+      cluster: '',
+    });
+
+    const channel = pusher.subscribe('chat');
+    channel.bind('message', (data) => {
+      this.messages.push(data);
+    });
+  }
+
+  submit(): void {
+    this.http
+      .post('http://localhost:8000/api/messages', {
+        username: this.username,
+        message: this.message,
+      })
+      .subscribe(() => (this.message = ''));
+  }
+}
